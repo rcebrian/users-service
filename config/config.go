79d31972c9d153096a3433a2285947d1e4610947
Rodrigo Cebrian Gonzalez <rodrigocebriang@gmail.com>
2022-10-04 00:45:33 +0200

    config env

--- config/config.go
+++ config/config.go
@@ -1,13 +1,10 @@
 package config
 
-const (
-	AppName = "api-template"
-)
-
 var AppConfig AppConf
 
 type AppConf struct {
-	LogLevel   string `envconfig:"LOG_LEVEL" default:"info"`
-	AppVersion string `envconfig:"APP_VERSION" default:"v1.0.0"`
-	HttpPort   int    `envconfig:"HTTP_PORT" default:"8080" `
+	LogLevel       string `envconfig:"LOG_LEVEL" default:"info"`
+	AppVersion     string `envconfig:"APP_VERSION" default:"v1.0.0"`
+	HttpHealthPort int    `envconfig:"HTTP_HEALTH_PORT" default:"8079"`
+	HttpPort       int    `envconfig:"HTTP_PORT" default:"8080"`
 }
